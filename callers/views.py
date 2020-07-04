829d948bd7d7e31337d479228ab9218c4f50ed60
PeterKipya <peter@africa-drilling-solutions.com>
2020-07-05 02:21:31 +0300

    Updated

--- callers/views.py
+++ callers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Caller
 from experts.models import Expert
@@ -8,14 +8,19 @@ from django import forms
 from django.contrib.auth.models import User
 from calls.models import Call
 
+
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Input a valid email address.')
+    first_name = forms.CharField(
+        max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(
+        max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(
+        max_length=254, help_text='Required. Input a valid email address.')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password1', 'password2', )
 
 
 def caller_signup(request):
@@ -26,62 +31,60 @@ def caller_signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            caller=Caller()
-            caller.user=user
+            caller = Caller()
+            caller.user = user
             caller.save()
             login(request, user)
             return redirect('/caller/')
         else:
-            return render(request, 'registration/c_signup.html',{'form':form})
+            return render(request, 'registration/c_signup.html', {'form': form})
     else:
         form = SignUpForm()
-        return render(request, 'registration/c_signup.html',{'form':form})
-
-
+        return render(request, 'registration/c_signup.html', {'form': form})
 
 
 def caller_login(request):
-    if request.method=="GET":
-        return render(request,"registration/c_login.html")
+    if request.method == "GET":
+        return render(request, "registration/c_login.html")
     else:
-        username=request.POST.get('username')
-        password=request.POST.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
         user = authenticate(username=username, password=password)
         if Caller.objects.filter(user=user).first():
             login(request, user)
             return redirect('/caller/')
         else:
-            return render(request,"registration/c_login.html",{'errors': "invalid credentials"})
+            return render(request, "registration/c_login.html", {'errors': "invalid credentials"})
 
 
 class CallView(View):
-	def get(self,request):
-		if request.user.is_authenticated:
-			caller=Caller.objects.filter(user=request.user).first()
-			if caller:
-				expert=Expert.objects.filter(is_free=True).first()
-				return render(request,"caller/calling_page.html",{'caller':caller,'expert':expert},None,None,None)
-			else:
-				return redirect("/caller/login")
-		else:
-			return redirect("/caller/login")
+    def get(self, request):
+        print("no receiver")
+        if request.user.is_authenticated:
+            caller = Caller.objects.filter(user=request.user).first()
+            if caller:
+                expert = Expert.objects.filter(is_free=True).first()
+                if expert:
+                    return render(request, "caller/calling_page.html", {'caller': caller, 'expert': expert}, None, None, None)
+                else:
+                    print("no receiver")
+                    calls = Call.objects.filter(caller=caller)
+                    return render(request, "caller/caller_home.html", {"caller": caller, "calls": calls, "message": "Our Fix team is fully occupied at the moment. Please try again in a few minutes!"}, None, None, None)
+            else:
+                    return redirect("/caller/login")
+        else:
+            return redirect("/caller/login")
 
 
 class CallerHome(View):
     def get(self, request):
         if request.user.is_authenticated:
-            caller=Caller.objects.filter(user=request.user).first()
+            caller = Caller.objects.filter(user=request.user).first()
             if caller:
-                calls=Call.objects.filter(caller=caller)
-                return render(request,"caller/caller_home.html",{"caller":caller,"calls":calls},None,None,None)
+                calls = Call.objects.filter(caller=caller)
+                return render(request, "caller/caller_home.html", {"caller": caller, "calls": calls}, None, None, None)
             else:
                 return redirect("/caller/login")
         else:
             return redirect("/caller/login")
-
-                
-            
-
-    
-    
