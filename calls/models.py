e8da6e3589ded6a5045e83693da4a2de3566602b
PeterKipya <peter@africa-drilling-solutions.com>
2020-07-05 17:33:13 +0300

    Updated

--- calls/models.py
+++ calls/models.py
@@ -13,7 +13,7 @@ class Call(models.Model):
 class Chat(models.Model):
 	id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	sender=models.ForeignKey(User,on_delete=models.CASCADE,related_name='sender')
-	call=models.ForeignKey(Call,on_delete=models.CASCADE)
+	call=models.ForeignKey(Call,on_delete=models.CASCADE,related_name="chat")
 	text=models.TextField(blank=True,null=True)
 	created = models.DateTimeField(auto_now_add=True, editable=False,blank=True,null=True)
 
