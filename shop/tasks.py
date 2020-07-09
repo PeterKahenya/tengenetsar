4ec46dd9e1c16599473d520a2d68cba64b0c1492
PeterKipya <peter@africa-drilling-solutions.com>
2020-07-09 22:59:27 +0300

    Sending Emails

--- shop/tasks.py
+++ shop/tasks.py
@@ -3,7 +3,6 @@ from celery import shared_task
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-from subscribers.models import Subscriber
 import uuid
 
 
